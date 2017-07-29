3f4609ebbec28d41e02605c3e77834739b4d1078
Bliznyuk Sergey <bliznyuk.sergey@gmail.com>
2017-07-29 20:40:37 +0300

    Initial commit

---
+++ src/hometask/TapeDeck.java
@@ -0,0 +1,14 @@
+package hometask;
+
+public class TapeDeck {
+
+    public boolean canRecord = false;
+
+    public void playTape() {
+        System.out.println("Пленка проигрывается");
+    }
+
+    public void recordTape() {
+        System.out.println("Идёт запись на пленку");
+    }
+}
---
+++ src/hometask1/DVDPlayer.java
@@ -0,0 +1,14 @@
+package hometask1;
+
+public class DVDPlayer {
+
+    boolean canRecord = false;
+
+    void recordDVD(){
+        System.out.println("Идёт запись DVD");
+    }
+
+    void playDVD(){
+        System.out.println("Диск проигрывается");
+    }
+}
